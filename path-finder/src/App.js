0e8d7306a335f72821b3176a868a6a08870fad18
arastumudgal <76526750+arastumudgal@users.noreply.github.com>
2021-09-04 13:27:58 +0530

    Add files via upload

---
+++ path-finder/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PATH FINDING VISUALIZER</h1>
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
